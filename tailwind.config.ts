296a7941c384daa9a379078d1cbcfcb1ec18a6c3
itsteatv <rezashow60@gmail.com>
2024-03-17 15:01:39 +0330

    feat(tailwind): Update Tailwind CSS configuration and add NextUI provider

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,12 @@
 import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react";
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    'node_modules/flowbite-react/lib/esm/**/*.js',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
@@ -16,8 +17,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [
-    require('flowbite/plugin'),
-  ],
+  plugins: [nextui()]
 };
 export default config;
