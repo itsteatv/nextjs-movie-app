9ed36ce39b62b2b579465184d261ce8802fe506a
itsteatv <rezashow60@gmail.com>
2024-03-19 01:52:04 +0330

    refactor(layout): Remove favicon.ico and update metadata in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,8 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Next.js 14 movie app",
   description: "Movie app built with Next.js 14",
+  icons:
+    "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🍿</text></svg>",
 };
 
 export default function RootLayout({
