5a4ffe4d1c03ee22ef2afbecc2d68501578d95f1
itsteatv <rezashow60@gmail.com>
2024-03-21 11:42:32 +0330

    refactor(database): Simplify MongoDB connection setup

--- utils/dbConfig.ts
+++ utils/dbConfig.ts
@@ -4,10 +4,7 @@ export async function connect() {
     if (mongoose.connections[0].readyState) return;
 
     try {
-        mongoose.connect(process.env.MONGO_URI!, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        mongoose.connect(process.env.MONGO_URI!);
         mongoose.connection.on("connected", () => {
             console.log("MongoDB connection established");
         });
