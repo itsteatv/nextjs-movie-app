f395340d58e1f2d77b2b33fb1c2e8bed92413af7
itsteatv <rezashow60@gmail.com>
2024-03-24 23:16:42 +0330

    refactor: Update MyList component to handle server-side session and redirection

--- app/mylist/page.tsx
+++ app/mylist/page.tsx
@@ -1,5 +1,13 @@
-const MyList = () => {
-  return <div>MyList</div>;
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+const MyList = async () => {
+  const session = await getServerSession();
+
+  if (!session) redirect("/signin");
+  return (
+    <div className="flex items-center justify-center min-h-screen">MyList</div>
+  );
 };
 
 export default MyList;
