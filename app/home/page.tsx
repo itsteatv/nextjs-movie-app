7ff12b9ab0d25658322e39738e1e4bb2441fd75e
itsteatv <rezashow60@gmail.com>
2024-03-26 00:20:19 +0330

    feat(home): Add Home component

---
+++ app/home/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen font-SourceCodePro font-bold text-1xl md:text-6xl sm:text-2xl">
+      Next.js 14 movie app
+    </div>
+  );
+};
+
+export default Home;
