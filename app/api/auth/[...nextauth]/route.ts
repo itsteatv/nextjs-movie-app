0e9d655bfda83bae404fe12d57f10d4b2f8537f3
itsteatv <rezashow60@gmail.com>
2024-03-25 22:35:48 +0330

    feat(auth): Add custom signIn callback for GitHub provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,9 +2,10 @@ import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import User from "@/models/User"
 import CredentialsProvider from "next-auth/providers/credentials"
-import { Account, User as AuthUser } from "next-auth"
+import { Account, User as AuthUser } from "next-auth";
 import { connect } from "@/utils/dbConfig"
 import bcrypt from "bcryptjs"
+import crypto from "crypto"
 
 export const authOptions: any = {
     providers: [
@@ -40,6 +41,34 @@ export const authOptions: any = {
             clientSecret: process.env.GITHUB_SECRET ?? "",
         }),
     ],
+    callbacks: {
+        async signIn({ user, account }: { user: AuthUser; account: Account }) {
+
+            if (account?.provider == "credentials") {
+                return true
+            }
+            if (account?.provider == "github") {
+                await connect();
+
+                try {
+                    const existingUser = await User.findOne({ email: user.email });
+
+                    if (!existingUser) {
+
+                        const generatedPassword = crypto.randomBytes(32).toString("base64");
+
+                        const newUser = new User({ email: user.email, password: generatedPassword });
+                        await newUser.save();
+                        return true;
+                    }
+
+                    return true;
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        }
+    }
 }
 
 export const handler = NextAuth(authOptions);
